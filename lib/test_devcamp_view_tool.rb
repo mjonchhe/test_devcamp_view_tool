2a99864ec7af8cd7a294e749faad2901d654547d
Manju  Jonchhe <mjonchhe@gmail.com>
2020-02-29 22:18:09 -0500

    Dev tool initial commit

---
+++ lib/test_devcamp_view_tool.rb
@@ -0,0 +1,7 @@
+require "test_devcamp_view_tool/version"
+require "test_devcamp_view_tool/renderer"
+
+module TestDevcampViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/test_devcamp_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TestDevcampViewTool do
+  it "has a version number" do
+    expect(TestDevcampViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
